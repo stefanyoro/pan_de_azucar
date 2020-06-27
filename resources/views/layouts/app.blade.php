b88ca1c5d5b07479fbff9147f37bec9e26db0c8f
odaurbina <36133888+odaurbina@users.noreply.github.com>
2020-06-27 01:30:04 -0400

    Vista de ver resultados y logo de pag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,13 @@
     <title>Pan de Azucar</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/logotipo.png')}}">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
-
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
-
     <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
-
-
     <link rel="stylesheet" href="{{ asset('fonts/ionicons/css/ionicons.min.css')}}">
     <link rel="stylesheet" href="{{ asset('fonts/fontawesome/css/font-awesome.min.css')}}">
 
--- resources/views/verResultados.blade.php
+++ resources/views/verResultados.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
- 
+@section('css')
+<link rel="stylesheet" href="{{asset('css/jquery.dataTables.min.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+@endsection 
 @section('content')
 
 <section class="section">
@@ -12,31 +16,155 @@
               <p  style="color:white; text-align:left;"> Resultados </p> 
             </div>
             <div class="card-body">
-              <div class="col-md-12">
-                selencione una carrera
-                <div class="card col-md-12">
+              <div class="card col-md-12">
                 <div class="card-body">
-                  <h5 class="card-title">Carrera</h5>
-                  <p class="card-text" align="center">...En construcción...</p>
-                </div>
-              </div>
+                  <h5 class="card-title">
+                    <div class="row">
+                      <div class="col-md-6"> Carrera:</div> 
+                        <!-- Button trigger modal -->
+                          <div class="col-md-6" align="right">
+                            <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#carrera" title="Directorio de carrera">
+                              <i class="fa fa-bicycle" aria-hidden="true"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="carrera" tabindex="-1" role="dialog" aria-labelledby="carreraModal" aria-hidden="true">
+                              <div class="modal-dialog" role="document" align="left">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="carreraModal">Selecciona la carrera</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    <div class="form-check form-check-inline">
+                                      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                      <label class="form-check-label" for="inlineRadio1">
+                                        Nombre de la carrera
+                                      </label>
+                                    </div>
+                                  <div style="padding: 50px 20px 20px 0px" align="center"><button type="button" class="btn btn-primary">Aceptar</button></div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        </h5>
+                        <p class="card-text" align="center">
+                          <input class="form-control form-control-lg" type="text" placeholder="..selencione una carrera..." disabled>
+                        </p>                  
+                      </div>
+                    </div>
+                <div class=" col-md-12" style="padding: 20px;">
+                
+                  <div class="row">
+                      <div class="col-md-12 table-responsive"> 
+                        <table id="listarcarrera" class="table">
+                          <thead>  
+                            <tr>
+                              <th > N◦</th>
+                              <th > Nombre</th>
+                              <th > Categoría</th>
+                              <th > Posicion</th>
+                              <th > Tiempo</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>12345</td>
+                              <td>Odalys Urbina</td>
+                              <td>Senior</td>
+                              <td>01</td>
+                              <td>
+                                <!-- Tiempo -->
+                                <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#exampleModal" title="Tiempo del corredor">
+                                 <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog " role="document">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Tiempos de: "Odalys Urbina"</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                        </button>
+                                      </div>
+                                      <!-- acción del botón  -->
+                                      <div class="modal-body">
+                                        <i class="fa fa-clock-o" aria-hidden="true"></i> Vuelta 1
+                                        <div class="tiempos col-md-12">
+                                          <input type="text" class="form-control tiempo" id="tiempo" name="tiempo[]" placeholder="tiempo" value="" disabled>
+                                        </div>                                 
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>                          
+                        </table>                      
+                        <div class="col-md-12" align="left">
+                          <div class="btn-group" role="group">
+                            <button type="button" style="background-color: #B03A2E;" title="PDF Carrera" class="btn btn-primary btn-sm">
+                              <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+                            </button>
+                          </div>
+                        </div>              
+                      </div>
+                    </div>  
 
               </div>
             </div> 
           </div>
         </div>
 
-        <div class="card col-md-5">
+        <div class="card col-md-5" align="center">
           <div class="card-body">
             <h1 class="card-title">Deja tu comentario</h1>
-            <p class="card-text" align="center">...En construcción...</p>
-            <a href="#" class="btn btn-primary" >Button</a>
+            <p class="card-text" >...En construcción...</p>
+            <a href="#" class="btn btn-primary" style="padding: 5px 85px">Comenta</a>
           </div>
         </div>
       </div>
     </div>
   </div>  
 </section>
-
-
+@endsection
+ @section('scriptJS')
+  <!--tabla de listar carreras-->
+  <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+  <script>
+       $(document).ready(function(){
+        //$("#listarcarrera").DataTable();
+        var tabla = $("#listarcarrera").DataTable( {
+          "language": {
+                "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+              "sFirst":    "Primero",
+              "sLast":     "Último",
+              "sNext":     "Siguiente",
+              "sPrevious": "Anterior"
+            },
+            "oAria": {
+              "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+              "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+          }
+        });
+      });
+  </script>
 @endsection
